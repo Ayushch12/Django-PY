8f73f10f7117a1276478f6f342cf6a90bc735cdb
Ayush chalisee <ayushchalise12@gmail.com>
2024-03-19 22:52:03 +0100

    Addition of two number

--- calc/urls.py
+++ calc/urls.py
@@ -4,7 +4,9 @@ from . import views  # Import the views module
 
 urlpatterns = [
     path('', views.home, name="home"),  # Define the path to the home view
-    path("admin/", admin.site.urls),    # Optionally, include the admin URLs
+    path('add',views.add, name ='add' )
+
+
 
 ]
 
--- calc/views.py
+++ calc/views.py
@@ -6,3 +6,15 @@ def home(request):
     # Return an HTTP response with the text "Hello World"
     return render(request,'home.html',{'name':'Ayush '})
 
+
+def add(request):
+
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+
+
+    # Return an HTTP response
+    return render(request,'result.html',{'result':res})
+
+
