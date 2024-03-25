6f81f57042b69f8080dba1d2e1cb65b0c7788c55
Ayush chalisee <ayushchalise12@gmail.com>
2024-03-26 00:07:23 +0100

    Adding and modifying the name,desc,price and more

--- travello/views.py
+++ travello/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 # Define your view function
+
+
 def index(request):
+
+    dest1 = Destination()
+    dest1.name = "Mumbai"
+    dest1.desc = "This is my City"
+    dest1.price = 700
+
+
     # Return an HTTP response with the text "Hello World"
-    return render(request,'index.html')
+    return render(request,'index.html',{'dest1': dest1})
