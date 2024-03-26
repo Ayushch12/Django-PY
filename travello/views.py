510ca3814027df121ec6ce96e975fa347172ea19
Ayush chalisee <ayushchalise12@gmail.com>
2024-03-26 11:05:03 +0100

    Added in the index

--- travello/views.py
+++ travello/views.py
@@ -11,7 +11,22 @@ def index(request):
     dest1.name = "Mumbai"
     dest1.desc = "This is my City"
     dest1.price = 700
+    dest1.img = "destination_1.jpg"
+
+    dest2 = Destination()
+    dest2.name = "India"
+    dest2.desc = "Nulla pretium tincidunt felis, nec."
+    dest2.price = 1000
+    dest2.img = "destination_2.jpg"
+
+    dest3 = Destination()
+    dest3.name = "Nepal"
+    dest3.desc = "Nulla pretium tincidunt felis, nec."
+    dest3.price = 12000
+    dest3.img = "destination_3.jpg"
+
+    dests = [dest1, dest2, dest3]
 
 
     # Return an HTTP response with the text "Hello World"
-    return render(request,'index.html',{'dest1': dest1})
+    return render(request,'index.html',{'dests': dests})
