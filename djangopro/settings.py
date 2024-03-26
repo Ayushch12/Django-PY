6952b352e675d80651416b5a3031e052adaeb705
Ayush chalisee <ayushchalise12@gmail.com>
2024-03-26 15:47:44 +0100

    Added Migration for PG admin

--- djangopro/settings.py
+++ djangopro/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "travello.apps.TravelloConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -77,8 +78,11 @@ WSGI_APPLICATION = "djangopro.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "telusko",
+        "USER": "postgres",
+        "PASSWORD": "Aayush",
+        "HOST": "localhost",
     }
 }
 
--- travello/views.py
+++ travello/views.py
@@ -12,18 +12,22 @@ def index(request):
     dest1.desc = "This is my City"
     dest1.price = 700
     dest1.img = "destination_1.jpg"
+    dest1.offer = False
+
 
     dest2 = Destination()
     dest2.name = "India"
     dest2.desc = "Nulla pretium tincidunt felis, nec."
     dest2.price = 1000
     dest2.img = "destination_2.jpg"
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = "Nepal"
     dest3.desc = "Nulla pretium tincidunt felis, nec."
     dest3.price = 12000
     dest3.img = "destination_3.jpg"
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
